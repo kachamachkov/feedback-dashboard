4c50ecb9aac5c1f2a299551e35c1991f41f043e4
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-17 16:13:59 +0200

    chore: improve db connection flow

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use(routes);
 
 mongoose.connect('mongodb://localhost:27017/feedback')
-    .then(() => console.log('DB Connected'));
-
-app.listen(3000, () => console.log('Server is listening on port 3000...'))
+    .then(() => {
+        console.log('Connected to the database.');
+        app.listen(3000, () => console.log('Server is listening on port 3000...'))
+    })
+    .catch(() => {
+        console.log('Connection to the database failed.');
+    });
