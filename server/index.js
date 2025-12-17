4bb90b12578905b07756da469ecaf70b27ae9e52
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-17 12:59:46 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// TODO: refactor to only allow a specific host port, when I know it
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello')
