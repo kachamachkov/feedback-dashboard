5b9e029c9893a181e4490e4a145b5a0c4e8f7fed
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-18 12:36:19 +0200

    add update and delete feedback endpoints

--- server/src/controllers/feedbackController.js
+++ server/src/controllers/feedbackController.js
@@ -7,12 +7,31 @@ router.post('/', async (req, res) => {
     const feedback = await feedbackService.create(feedbackData);
 
     res.json(feedback);
-})
+});
 
 router.get('/', async (req, res) => {
     const feedbacks = await feedbackService.getAll();
 
     res.json(feedbacks);
-})
+});
+
+router.get('/:feedbackId', async (req, res) => {
+    const feedback = await feedbackService.getOne(req.params.feedbackId);
+
+    res.json(feedback);
+});
+
+router.put('/:feedbackId', async (req, res) => {
+    const feedbackData = req.body;
+    const feedback = await feedbackService.update(req.params.feedbackId, feedbackData);
+
+    res.json(feedback);
+});
+
+router.delete('/:feedbackId', async (req, res) => {
+    await feedbackService.delete(req.params.feedbackId);
+
+    res.status(204).json({ok: true});
+});
 
 module.exports = router;
--- server/src/services/feedbackService.js
+++ server/src/services/feedbackService.js
@@ -2,4 +2,10 @@ const Feedback = require('../models/Feedback');
 
 exports.getAll = () => Feedback.find();
 
+exports.getOne = (feedbackId) => Feedback.findById(feedbackId);
+
+exports.update = (feedbackId, feedbackData) => Feedback.findByIdAndUpdate(feedbackId, feedbackData);
+
 exports.create = async (feedbackData) => Feedback.create(feedbackData);
+
+exports.delete = async (feedbackId) => Feedback.findByIdAndDelete(feedbackId);
